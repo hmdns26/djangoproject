426332dbe3d48ebdf97b50255f409651410a7d5f
hamed nasiri <hmd996hh@gmail.com>
2025-08-23 13:02:52 +0330

    add somefiture

--- store/serializers.py
+++ store/serializers.py
@@ -67,8 +67,6 @@ class CartSerializer(serializers.ModelSerializer):
     def get_total_price(self,cart):
         return sum([item.quantity *item.product.unit_price for item in cart.items.all()])
 
-    
-     
 class ChangeCartitemSerializer(serializers.ModelSerializer):
     class Meta:
         model=CartItem
